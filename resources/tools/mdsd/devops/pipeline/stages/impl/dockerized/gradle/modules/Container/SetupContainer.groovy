40269e5644dfa9453bc3cf1a01e7b67ced5e92f1
Sebastian Krach <krach@fzi.de>
2019-07-25 11:58:23 +0200

    Fixed faulty variable access

--- resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/gradle/modules/Container/SetupContainer.groovy
+++ resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/gradle/modules/Container/SetupContainer.groovy
@@ -2,7 +2,7 @@ def gradleVersion = CFG.gradleVersion ?: '5.5.1'
 def gradleJdkVersion = CFG.gradleJdkVersion ?: '11'
 
 extendConfiguration([
-    dockerBuildImage: "gradle:${CFG.gradleVersion}-jdk${CFG.mavenJdkVersion}",
+    dockerBuildImage: "gradle:${gradleVersion}-jdk${gradleJdkVersion}",
     dockerWithRunParameters: """\
     ${CFG.dockerWithRunParameters ?: ""} \
     -v ${CFG.slaveHome}/.gradle:/.gradle:ro
