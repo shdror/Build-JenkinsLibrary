591f0e7c09f0c2947fe1d95b3a7a115360a1dda7
Stephan Seifermann <stephan.seifermann@kit.edu>
2019-08-26 17:12:51 +0200

    Fixed dockerized maven build.

--- resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/maven/modules/Build/Build.groovy
+++ resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/maven/modules/Build/Build.groovy
@@ -1,2 +1,23 @@
-MPLModule('Prepare Empty Folder For Build')
-MPLModule('Apply Settings And Build')
+extendConfiguration([
+    emptySlaveDir: "/tmp/${env.BUILD_TAG}"])
+
+CFG = getCurrentConfiguration()
+
+MPLModulePostStep {
+    sh "rm -rf \"${CFG.emptySlaveDir}\""
+}
+
+sh "mkdir -p \"${CFG.emptySlaveDir}\""
+
+def mavenSettingsFile = "${CFG.emptySlaveDir}/emptyFile"
+
+if (CFG.mavenSettingsId) {
+    configFileProvider(
+            [configFile(fileId: CFG.mavenSettingsId, variable: 'MAVEN_SETTINGS')]) {
+                extendConfiguration([mavenSettingsFile: MAVEN_SETTINGS])
+                MPLModule("Build")
+            }
+} else {
+    sh "touch ${mavenSettingsFile}"
+    MPLModule("Build")
+}
