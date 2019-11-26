6a0d3b95cdc813f78c1b9682f94749cfc0341468
Stephan Seifermann <9149072+seiferma@users.noreply.github.com>
2019-11-26 21:20:01 +0100

    Added missing backslash

--- resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/modules/Container/SetupContainer.groovy
+++ resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/modules/Container/SetupContainer.groovy
@@ -2,7 +2,7 @@ extendConfiguration([dockerWithRunParameters: """\
         ${CFG.dockerWithRunParameters ?: ""} \
         -u ${CFG.slaveUid} \
         -v ${CFG.workspacePath}:/ws:ro \
-        -e "RELEASE=${CFG.deployRelease}"
+        -e "RELEASE=${CFG.deployRelease}" \
         --network proxy \
         -it \
         --entrypoint=/bin/cat \
