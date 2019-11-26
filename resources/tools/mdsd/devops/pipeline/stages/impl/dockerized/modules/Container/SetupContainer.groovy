d2810b553c44039331a71dfeeeb8172323977776
Stephan Seifermann <9149072+seiferma@users.noreply.github.com>
2019-11-26 21:17:53 +0100

    Pass release flag to docker container

--- resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/modules/Container/SetupContainer.groovy
+++ resources/tools/mdsd/devops/pipeline/stages/impl/dockerized/modules/Container/SetupContainer.groovy
@@ -2,6 +2,7 @@ extendConfiguration([dockerWithRunParameters: """\
         ${CFG.dockerWithRunParameters ?: ""} \
         -u ${CFG.slaveUid} \
         -v ${CFG.workspacePath}:/ws:ro \
+        -e "RELEASE=${CFG.deployRelease}"
         --network proxy \
         -it \
         --entrypoint=/bin/cat \
